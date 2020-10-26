a06c4536f13260e68a6252672a7088710eadfe5e
todd.german <todd.german@fathym.com>
2020-10-26 12:20:20 -0600

    refactored and added tests project

---
+++ state-api-replace-this-tests/Host/ConnectToStateTests.cs
@@ -0,0 +1,43 @@
+using Fathym.Testing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace state_api_replace_this_tests
+{
+    [TestClass]
+    public class ConnectToStateTests : AzFunctionTestBase
+    {
+        
+        public ConnectToStateTests() : base()
+        {
+            APIRoute = "api/ConnectToState";                
+        }
+
+        [TestMethod]
+        public async Task TestConnectToState()
+        {
+            LcuEntApiKey = "";            
+            PrincipalId = "";
+
+            addRequestHeaders();
+
+            var url = $"{HostURL}/{APIRoute}";            
+
+            var response = await httpGet(url); 
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+            var content = await getContent<BaseResponse<dynamic>>(response);
+
+            Assert.AreEqual(Status.Success, content.Status);           
+
+            throw new NotImplementedException("Implement me!");                  
+        }
+    }
+}
